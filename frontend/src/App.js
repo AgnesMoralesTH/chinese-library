e95621a6f1e204d9297f689e323565e5fef014e4
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-06-30 17:36:19 -0700

    added a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1>Princess Momo</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            <div className="navers">
+            <div className="logo">
+                <h1>万</h1>
+            </div>
+
+            <div className="nav-options">
+                <ul>
+                    <li>HOME</li>
+                    <li>LIBRARIES</li>
+
+                    <li>MONICA</li>
+                </ul>
+            </div>
+             
+        </div>
+
+        <hr></hr>
+        </div>
+    )
+}
+
+export default NavBar
